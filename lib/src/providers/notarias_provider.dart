2c17a56e13f1ac21302c10d3340615d90ac46fdd
awss197812 <wsalcedo78@gmail.com>
2020-02-04 23:04:39 -0500

    Rest de notarias

--- lib/src/providers/notarias_provider.dart
+++ lib/src/providers/notarias_provider.dart
@@ -4,10 +4,11 @@ import 'package:http/http.dart' as http;
 import 'package:notarias/src/models/notaria_model.dart';
 
 class NotariasProvider {
-  String _url = 'https://directorionotarial.com/notarias-internet/api/v1.0/mysql';
+  //String _url = 'directorionotarial.com/notarias-internet/api/v1.0/mysql/';
+  String _url = 'directorionotarial.com';
 
   Future<List<Notaria>> getNotarias() async {
-    final url = Uri.https(_url, 'notarias.php');
+    final url = Uri.https(_url, 'notarias-internet/api/v1.0/mysql/notarias.php');
     final resp = await http.get(url);
     final decodedData = json.decode(resp.body);
     print(decodedData);
