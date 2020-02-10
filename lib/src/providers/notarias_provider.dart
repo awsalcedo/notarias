e9d474995be866b93969e8ec8c0c66cfb26008bb
mios260410 <mios260410@iMac-de-desarrollo.local>
2020-02-10 16:52:19 -0500

    Mapeo a entidad Notaria

--- lib/src/providers/notarias_provider.dart
+++ lib/src/providers/notarias_provider.dart
@@ -1,18 +1,38 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:notarias/src/models/notaria_model.dart';
 
 class NotariasProvider {
   //String _url = 'directorionotarial.com/notarias-internet/api/v1.0/mysql/';
   String _url = 'directorionotarial.com';
 
+  void initNotarias () {
+    //Borrar un localstorage antiguo de provincias
+
+    _cargarNotarias();
+  }
+
   Future<List<Notaria>> getNotarias() async {
     final url = Uri.https(_url, 'notarias-internet/api/v1.0/mysql/notarias.php');
     final resp = await http.get(url);
-    final decodedData = json.decode(resp.body);
-    print(decodedData);
+
+    final notaria = notariaFromJson(resp.body); //mapea a la entidad
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('notarias', resp.body);
+
+    
+    print(notaria[1].notariosPasivos[0].nombre);
+
+
     return [];
+  }
+
+  void _cargarNotarias() {
+    //obtener las notarias del sharedPreference
+    
   }
 
 
